8a949670c7f148a36aeef05cce830c4d7b5dc28b
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-05 15:27:50 -0400

    Pico Rivera 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -145,7 +145,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                 social_media: [
                     {
                         name: "Facebook",
-                        url: "https://www.facebook.com/creeksidetowncenter/",
+                        url: "https://www.facebook.com/ShopPico/",
                         iconClass: "fab fa-facebook"
                     },
                     // {
