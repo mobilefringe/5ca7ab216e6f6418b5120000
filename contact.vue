8671e256e4f6b66fcae674086eb062139619c6b3
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-12 11:48:16 -0400

    Pico Rivera 2.0 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -145,7 +145,7 @@
                 loadData: async function () {
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     try {
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/creekside-contact-us.json" }), this.$store.dispatch("getData", "repos")]);
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/pico-contact-us.json" }), this.$store.dispatch("getData", "repos")]);
                         console.log(results)
                         return results;
                     } catch (e) {
