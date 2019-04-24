7fec500fe8cb8b2676e4cd28c09b95d363161113
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-24 12:21:17 -0400

    Pico Rivera 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -192,7 +192,6 @@
                     
                     subcategories = _.map(subcategories, 'name').sort();
                     subcategories.unshift('All Cuisine');
-                    console.log(subcategories)
                     return subcategories;
                 },
                 filterByCategory() {
