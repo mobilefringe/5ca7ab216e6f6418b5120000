8d1975cdc1cf62def889babbd05e3fca1a4fb3b7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-24 11:28:47 -0400

    Pico Rivera 2.0 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -36,8 +36,8 @@
                             <div class="col-md-12">
                                 <div class="row margin_30">
                                     <div class="col-md-12">
-                                        <router-link to="/promotions">
-                    		                <div class="animated_btn pull-left">Back to Sales & Promotions</div>    
+                                        <router-link to="/events-and-promotions">
+                    		                <div class="animated_btn pull-left">Back to Events & Promotions</div>    
                     		            </router-link>    
                                     </div>
                                 </div>
