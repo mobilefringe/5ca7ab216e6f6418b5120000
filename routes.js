9e84eb8390c17cce6789b959c58683aad5d102ff
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-12 13:00:01 -0400

    Pico Rivera 2.0 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -30,7 +30,29 @@ define([], function () {
                         }
                     ]
                 },
-                
+                {
+                   path: '/jobs',
+                   component: view('default'),
+                   meta: {
+                      breadcrumb: 'Jobs'
+                   },
+                   children: [
+                      {
+                         path: '',
+                         component: view('jobs'),
+                         name: 'jobList'
+                      },
+                      {
+                         path: ':id',
+                         component: view('job_details'),
+                         meta: {
+                            breadcrumb: 'Jobs'
+                         },
+                         name: 'jobDetails',
+                         props: true
+                      }
+                   ]
+                },
                 {
                     path: '/leasing',
                     component: view('default'),
