98c8dee20212c8214a6e30206bb04e9ec856dbce
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2019-07-30 17:40:06 -0400

    Pico Rivera 2.0 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -71,7 +71,7 @@
                                         </label>
         						    </div>
         						    <div class="col-xs-12">
-        						        <p>For more details about personal privacy, please read our <a href="" target="_blank">Privacy Policy</a>.</p>
+        						        <p>For more details about personal privacy, please read our <a href="/pages/pico-privacy-policy" target="_blank">Privacy Policy</a>.</p>
                                     </div>
                                 </div>
                                 <div class="form-group">
