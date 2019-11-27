cdbe9e5f687c4f4db5a19eea0a33d81d616bcb4d
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-27 11:23:46 -0500

    Pico Rivera 2.0 1.2  /dine.vue

--- dine.vue
+++ dine.vue
@@ -56,12 +56,12 @@
                             	        <router-link :to="'/stores/'+ store.slug">
                                 			<!--<img class="store_img" :src="store.image_url" alt="">-->
                                 			<div v-if="!store.no_store_logo">
-                                			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="Logo">
-                                			    <img  class="store_img" :src="store.store_front_url_abs" alt="">
+                                			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
+                                			    <img  class="store_img" :src="store.store_front_url_abs" :alt="store.name">
                                 			</div>
                                 			
                                             <div v-else class="no_logo_container">
-                                                <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="store logo">
+                                                <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                                                 <div class="no_logo_text">
                                                     <div class="store_text"><h4>{{ store.name }}</h4></div>
                                                 </div>
