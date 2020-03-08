09da0380e50f7c93c2075125656181b54e2defd4
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-07 23:27:54 -0500

    Pico Rivera 2.0 1.2  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -262,7 +262,7 @@
                         this.storeHours = _.sortBy(storeHours, function(o) { return o.day_of_week });
                         
                         // DELIVERY
-                        var delivery_category = 8233;
+                        var delivery_category = 8885;
                         var categories = this.currentStore.categories;
                         var subcategories = this.currentStore.subcategories;
                         if (_.includes(categories, delivery_category) && !_.isEmpty(subcategories)) {
